101d6b553cc0b06b01b0d96ba24a2607718a5309
alMaruf <almarufinfo@gmail.com>
2025-06-24 02:54:05 +0600

    connect mysql database and update code

--- selenium_quotes.py
+++ selenium_quotes.py
@@ -1,40 +1,50 @@
+import mysql.connector
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
-import csv
 
+db = mysql.connector.connect(
+    host ="localhost",
+    user="root",
+    password="",
+    database="quotes_db"
+)
+
+cursor = db.cursor()
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-url="https://quotes.toscrape.com/js"
-driver.get(url)
+driver.get('https://quotes.toscrape.com/js')
 time.sleep(3)
 
 
-with open('quotes.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Quote', 'Author'])
     
-    while True:
-        quotes = driver.find_elements(By.CLASS_NAME, "quote")
+while True:
+    
+     quotes = driver.find_elements(By.CLASS_NAME, "quote")
         
-        for quote in quotes:
-            text = quote.find_element(By.CLASS_NAME, "text").text
-            author= quote.find_element(By.CLASS_NAME, "author").text
-            print(f"{(text)} - {(author)}")
+     for quote in quotes:
+        text = quote.find_element(By.CLASS_NAME, "text").text
+        author= quote.find_element(By.CLASS_NAME, "author").text
+        print(f"{(text)} - {(author)}")
             
-            writer.writerow([text, author])
+        # writer.writerow([text, author])
             
-        try:
-            next_button = driver.find_element(By.CSS_SELECTOR, 'li.next > a')
-            next_button.click()
-            time.sleep(3)
             
-        except:
-            print("All pages scraped successfully!")
-            break        
+        sql = "INSERT INTO quotes (quote, author) VALUES (%s, %s)"
+        val =(text, author)
+        cursor.execute(sql, val)
+        db.commit()
+            
+     try:
+        next_button = driver.find_element(By.CSS_SELECTOR, 'li.next > a')
+        next_button.click()
+        time.sleep(3)
+            
+     except:
+        print("All pages scraped successfully!")
+        break        
 
         
     
@@ -42,3 +52,5 @@ with open('quotes.csv', 'w', newline='', encoding='utf-8') as file:
 
     
 driver.quit()
+cursor.close()
+db.close()
